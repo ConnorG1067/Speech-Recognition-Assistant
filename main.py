8406b5d75d91e595c6e4ff49a07407e34fe682e6
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2021-01-31 19:16:25 -0500

    audio response from computer

--- main.py
+++ main.py
@@ -10,11 +10,26 @@ hear = sr.Recognizer()
 
 mic = sr.Microphone(device_index=1)
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={YourCity}&appid={Your API}&units=metric'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Ottawa&appid=afd36b72a1089a148c582bc930a4267d&units=metric'
 obtain = requests.get(url)
 data = obtain.json()
 temp =  data['main']['temp']
 
+import speech_recognition as sr
+import pyaudio
+import time
+import requests
+import json
+import webbrowser
+import pyttsx3
+
+hear = sr.Recognizer()
+#print(sr.Microphone.list_microphone_names())
+
+mic = sr.Microphone(device_index=1)
+
+
+engine = pyttsx3.init()
 
 
 while True:
@@ -22,33 +37,56 @@ while True:
         audio = hear.listen(source)
         
         text = hear.recognize_google(audio)
-       
+            
         if(text) == "hello":
             print("Hello User")
-        
+            engine.say("Hello User")
+            engine.runAndWait()
+            
         elif(text) == "help":
             print("Commands for voice assistant include\n1. Hello --> Hello User\n2. Help --> provide commands for assistant\n3. What time is it --> provides local time and date\n4. What's the tempature --> provides tempature in selected city\n5. Tell me a joke --> Returns a joke\n6. How are you --> provides a response\n7. Open browser --> Opens Google\n8. Open video --> Opens Youtube\n9. Exit --> Exits assistant")
+            engine.say("Commands for voice assistant include\n1. Hello --> Hello User\n2. Help --> provide commands for assistant\n3. What time is it --> provides local time and date\n4. What's the tempature --> provides tempature in selected city\n5. Tell me a joke --> Returns a joke\n6. How are you --> provides a response\n7. Open browser --> Opens Google\n8. Open video --> Opens Youtube\n9. Exit --> Exits assistant")
+            engine.runAndWait()
         
         elif(text) == "what time is it":
             print("The current date/time is " + time.ctime())
+            engine.say("The current date/time is " + time.ctime())
+            engine.runAndWait()
         
         elif(text) == "what's the temperature":
             print("The current temperature in your city is " + str(temp) + " degrees celsius")
-        
+            engine.say("The current temperature in your city is " + str(temp) + " degrees celsius")
+            engine.runAndWait()
+
+
         elif(text) == "tell me a joke":
             print("Yesterday I saw a guy spill all his Scrabble letters on the road, So I asked him, “What’s the word on the street?”")
-        
+            engine.say("Yesterday I saw a guy spill all his Scrabble letters on the road, So I asked him, “What’s the word on the street?”")
+            engine.runAndWait()
+
+
         elif(text) == "how are you":
             print("I do not know because I am a computer. I do not have emotions.")
-        
+            engine.say("I do not know because I am a computer. I do not have emotions.")
+            engine.runAndWait()
+
+
         elif(text) == "open browser":
             webbrowser.open('www.google.com')
-       
+            engine.say("Browser opened")
+            engine.runAndWait()
+
+
         elif(text) == "open video":
             webbrowser.open('www.youtube.com')
-        
+            engine.say("Youtube opened")
+            engine.runAndWait()
+
+
         elif(text) == "exit":
+            engine.say("Exiting")
+            engine.runAndWait()
             exit()
-        
+
         
         
--- test.py
+++ test.py
@@ -1,3 +1,36 @@
+import speech_recognition as sr
+import pyaudio
+import time
+import requests
+import json
 import webbrowser
-
-webbrowser.open('www.google.com')
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+ 
+ 
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+ 
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+ 
+        except Exception as e:
+            print("Expection:"+str(e))
+    return said
+ 
+ 
+text = get_audio()
+ 
+if "DC" in text:
+    print("https://discord.gg/RjnpzX3")
+ 
+ 
+if "how are you" in text:
+    speak("I am fine ")
